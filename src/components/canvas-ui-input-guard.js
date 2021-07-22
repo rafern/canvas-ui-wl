7ceb2e1072d79caa196ee3598a76feefaa610dc6
rafern <rafern@protonmail.com>
2021-07-22 14:09:48 +0100

    Make canvas-ui-input-guard disable components, not objects

--- src/components/canvas-ui-input-guard.js
+++ src/components/canvas-ui-input-guard.js
@@ -3,29 +3,59 @@ import { WLRoot } from '../core/WLRoot';
 /*global WL*/
 
 WL.registerComponent('canvas-ui-input-guard', {
-    /** (optional) Object to disable if keyboard is in use */
+    /** (optional) Name of component to disable if keyboard is in use */
+    keyboardComponentName: {type: WL.Type.String, default: null},
+    /** (optional) Object containing component to disable if keyboard is in use. Required if keyboardComponentName is set, else, ignored */
     keyboardObject: {type: WL.Type.Object, default: null},
-    /** (optional) Object to disable if pointer is hovering a UI root */
+    /** (optional) Name of component to disable if pointer is hovering a UI root is in use */
+    pointerComponentName: {type: WL.Type.String, default: null},
+    /** (optional) Object containing component to disable if pointer is hovering a UI root. Required if pointerComponentName is set, else, ignored */
     pointerObject: {type: WL.Type.Object, default: null},
-    /** (optional) Object which has a cursor component. Needed if pointerObject is set */
+    /** (optional) Object which has a cursor component. Required if pointerObject is set, else, ignored */
     cursorObject: {type: WL.Type.Object, default:null},
 }, {
     init() {
         this.pointer = null;
+        this.pointerComponent = null;
+        this.keyboardComponent = null;
     },
     start() {
-        if(this.pointerObject !== null) {
-            if(this.cursorObject === null) {
-                console.warn('pointerObject set in canvas-ui-keyboard-guard, but cursorObject was not');
-                return;
+        if(this.keyboardComponentName !== null) {
+            if(this.keyboardObject !== null) {
+                const keyboardComponent = this.keyboardObject.getComponent(this.keyboardComponentName);
+                if(keyboardComponent === null)
+                    console.warn('keyboardObject has no component with name', this.keyboardComponentName);
+                else
+                    this.keyboardComponent = keyboardComponent;
             }
-
-            const cursor = this.cursorObject.getComponent('cursor');
-            if(cursor === null)
-                console.warn('cursorObject set in canvas-ui-keyboard-guard, but cursorObject has no cursor component');
             else
-                this.pointer = WLRoot.getPointerID(cursor);
+                console.warn('keyboardComponentName set in canvas-ui-keyboard-guard, but keyboardObject was not');
         }
+
+        if(this.pointerComponentName !== null) {
+            if(this.pointerObject !== null) {
+                const pointerComponent = this.pointerObject.getComponent(this.pointerComponentName);
+                if(pointerComponent === null) {
+                    console.warn('pointerObject has no component with name', this.pointerComponentName);
+                    return;
+                }
+
+                if(this.cursorObject !== null) {
+                    const cursor = this.cursorObject.getComponent('cursor');
+                    if(cursor === null)
+                        console.warn('cursorObject set in canvas-ui-keyboard-guard, but cursorObject has no cursor component');
+                    else {
+                        this.pointer = WLRoot.getPointerID(cursor);
+                        this.pointerComponent = pointerComponent;
+                    }
+                }
+                else
+                    console.warn('pointerObject set in canvas-ui-keyboard-guard, but cursorObject was not');
+            }
+            else
+                console.warn('pointerComponentName set in canvas-ui-keyboard-guard, but pointerObject was not');
+        }
+
     },
     update(_dt) {
         if(this.keyboardObject !== null) {
